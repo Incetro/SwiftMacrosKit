b9daac6c9c46e0ec5490ee6bd7c3084cd532780c
Gleb-Kovalenko <80492878+Gleb-Kovalenko@users.noreply.github.com>
2025-08-15 15:38:48 +0300

    feat: update dao version

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.1.1"),
-        .package(url: "https://github.com/Incetro/DAO.git", from: "2.5.5")
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "509.1.1"),
+        .package(url: "https://github.com/Incetro/DAO.git", from: "3.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
